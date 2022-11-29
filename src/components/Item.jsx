36e680dc80e26579f40c66f74f7b6fd72972d116
Br1-NT <magaro.bruno@gmail.com>
2022-11-29 13:01:14 -0300

    itemCard styles

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import imgA from '../assets/img/0a.jpg';
 import imgB from '../assets/img/0b.jpg';
+import '../css/Item.css';
+
 export function Item() {
     return(
         <div className='item'>
-            <img src={imgA} alt="foto producto" className='item__img imgA'/>
-            <img src={imgB} alt="foto producto" className='item__img imgB'/>
-            <div className='item__info'>  
-                <p>Notebook Asus TUF</p>
-                <p>Descripción: <i>i5 - 16GB RAM - SSD 512GB - RTX 3050 - SO W11</i></p>
-                <p>$ 425.000</p>
-                <p>En Stock</p>
+            <div className='item__img-container'>
+                <img src={imgA} alt="foto producto" className='item__img imgA'/>
+                <img src={imgB} alt="foto producto" className='item__img imgB'/>
+                <p className="item__price-off">15 % OFF</p>
             </div>
-            <button>+Carrito</button>
+            <div className='item__info'>  
+                <p className='item__info--title'>Notebook Asus TUF</p>
+                <p>Descripción: <i>i5 - 16GB RAM - SSD 512GB - RTX 3050 - SO W11</i></p>
+                <p className='item__info--price'>$ 425.000</p>
+                <p className='item__info--stock'>En Stock</p>
+            </div>
+            <button className='item__btn'>+Agregar al Carrito</button>
         </div>
     );
 };
