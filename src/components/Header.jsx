6d48df5259cd266741e184f45d3cc429aa35a9ff
Br1-NT <magaro.bruno@gmail.com>
2022-12-03 13:00:52 -0300

    navStyle & rrss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import imgLogo from '../assets/logo-tecnolandia.png';
 import { FormSearch } from "./FormSearch";
 import '../css/Header-Form.css';
+import { SlSocialInstagram, SlSocialTwitter } from 'react-icons/sl';
+import { FaWhatsapp } from 'react-icons/fa';
 
 export function Header({ searchTxt, setSearchTxt } ) {
     return(
@@ -11,6 +13,11 @@ export function Header({ searchTxt, setSearchTxt } ) {
                 <h1 className='header__title'>Tecnolandia</h1>
             </div>
             <FormSearch className='header__form' searchTxt={searchTxt} setSearchTxt={setSearchTxt} />
+            <div className='header__rrss' >
+                <SlSocialInstagram size={'20px'} className='rrss-icon' />
+                <SlSocialTwitter size={'20px'} className='rrss-icon' />
+                <FaWhatsapp size={'20px'} className='rrss-icon' />
+            </div>
         </header>
     );
 };
