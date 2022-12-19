17a899b7520cffa8fe0f248bc26d0925c3025e97
Br1-NT <magaro.bruno@gmail.com>
2022-12-19 16:31:44 -0300

    style detailItem

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import '../css/ItemDetailContainer.css';
+import { TbTruckDelivery } from 'react-icons/tb';
 export function ItemDetailContainer({ data }) {
     let { id } = useParams();
 
     const item = data.find(item => item.id === parseInt(id))
 
+    const offPrice = () => {
+        const price = item.price;
+        const off = item.off
+        return price - ((price * off) / 100)
+    }
+
     return (
-        <div>
+        <div className='item-detail'>
             <h2>{item.name}</h2>
-            <img src={item.imgA} alt="imagen item" />
-            <p><strong>Fabricante: </strong>{item.description.maker}</p>
-            <p><strong>Procesador: </strong>{item.description.processor}</p>
-            <p><strong>Memoria RAM: </strong>{item.description.ram} GB</p>
-            <p><strong>Memoria SSD </strong>{item.description.memory} GB</p>
-            <p><strong>Sistema Operativo: </strong>{item.description.so}</p>
+            <div className='item-detail__info'>
+                <img src={item.imgA} alt="imagen item" />
+                <div className='item-detail__info--features'>
+                    <p><strong>Fabricante: </strong>{item.description.maker}</p>
+                    <p><strong>Procesador: </strong>{item.description.processor}</p>
+                    <p><strong>Memoria RAM: </strong>{item.description.ram} GB</p>
+                    <p><strong>Memoria SSD </strong>{item.description.memory} GB</p>
+                    <p><strong>Sistema Operativo: </strong>{item.description.so}</p>
+                    <p><strong>Precio: </strong><strike>${item.price}</strike> <strong>${offPrice()}</strong></p>
+                </div>
+            </div>
+            <div className='item-detail__delibery'>
+                <TbTruckDelivery size={'25px'} />
+                <p>Envio gratis a todo el pais</p>
+            </div>
+
         </div>
     )
 }
