f1906552d65e962093cb109bad6a7ce2e4b0bcf2
Br1-NT <magaro.bruno@gmail.com>
2023-01-12 11:18:53 -0300

    footer img & deleteToCart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,8 +3,8 @@ import { useContext } from "react";
 import { cartContext } from "../context/CartContextProvider";
 
 export function Cart() {
-    const { cart, totalCart } = useContext(cartContext);
-    console.log(totalCart);
+    const { cart, totalCart, deleteToCart } = useContext(cartContext);
+    //console.log(totalCart);
     const offPrice = (price, off) => {
         return price - ((price * off) / 100)
     }
@@ -18,12 +18,14 @@ export function Cart() {
                 cart.map(item => {
                     const totalPrice = quantifyProductTotal(offPrice(item.price, item.off), item.quantify)
                     return <div key={item.id}>
+                        <img src={item.img} alt="" />
                         <h3>{item.name}</h3>
                         <p>${item.price}</p>
                         <p>{item.off}%</p>
                         <p>$ {offPrice(item.price, item.off)}</p>
                         <p>Cant: {item.quantify}</p>
                         <h4>Total: $ {totalPrice}</h4>
+                        <button onClick={() => { deleteToCart(item.id, totalPrice) }}>X</button>
                     </div>
                 })
             }
--- src/context/CartContextProvider.jsx
+++ src/context/CartContextProvider.jsx
@@ -8,6 +8,10 @@ export function CartContextProvider({ defaultValue = [], children }) {
     const [cart, setCart] = useState(defaultValue);
     const [totalCart, setTotalCart] = useState(0);
 
+    const isInCart = id => { //check para ver si producto ya existe en cart
+        return cart.find(product => product.id === id)
+    }
+
     const addToCart = (id, name, quantify, img, price, off) => {
 
         if (isInCart(id)) { //verifica si existe el producto(id) en el cart
@@ -35,13 +39,18 @@ export function CartContextProvider({ defaultValue = [], children }) {
         setTotalCart(totalCart + ((price - (price * off) / 100) * quantify))
     };
 
+    const deleteToCart = (id, priceProduct) => {
+        console.log('Producto ' + id + ' eliminad del CARRITO' + 'Precio: ' + priceProduct);
 
-    const isInCart = id => {
-        return cart.find(product => product.id === id)
+        const copyCart = [...cart];
+        setCart(copyCart.filter(item => item.id !== id)); // Filtra producto segun id para descartarlo del cart
+
+        setTotalCart(totalCart - priceProduct);//actualiza total de cart, descontando valor de producto pasado por id
+
     }
 
     return (
-        <cartContext.Provider value={{ addToCart, cart, totalCart }}>
+        <cartContext.Provider value={{ addToCart, deleteToCart, cart, totalCart }}>
             {children}
         </cartContext.Provider>
     )
